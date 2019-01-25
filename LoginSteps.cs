dfdda69f6dbb37defce687fdef8fdee8c5bdda1f
muyiiwa babatunde <46856879+muyibabz@users.noreply.github.com>
2019-01-25 19:48:56 +0000

    Add files via upload

---
+++ LoginSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Project5
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [When(@"I enter invalid username")]
+        public void WhenIEnterInvalidUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
